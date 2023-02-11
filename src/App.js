86b00b3f457ac16db26e98c9d09cf29808f2a62d
emo_new_bug <emotupev11@gmail.com>
2023-02-11 18:42:21 +0200

    feat: implement click counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, {useState, useEffect} from 'react';
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  useEffect(() => {
+    document.title = `Count (${counter})`
+  }, [counter])
+
   return (
     <div className="App">
       <section class="hero">
@@ -12,6 +19,7 @@ function App() {
       <div class="container is-fullhd">
         <div class="notification">
           Edit the <code>./src</code> folder to add components.
+          <button onClick={() => setCounter(counter + 1)}>Count ({counter})</button>
         </div>
       </div>
     </div>
